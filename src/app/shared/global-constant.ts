229e3ee950708da971e2b258bf7200c390b33d3e
WCHYI-CHEESE <102250400+WCHYI-CHEESE@users.noreply.github.com>
2024-01-14 21:18:47 +0800

    initial commit

---
+++ src/app/shared/global-constant.ts
@@ -0,0 +1,13 @@
+export class GlobalConstants {
+  //message
+  public static genericError: string =
+    'Something went wrong. Please try agian later';
+
+  //Regex
+  public static nameRegex: string = '[a-zA-Z0-9 ]*';
+  public static emailRegex: string =
+    '[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}';
+
+  //Variable
+  public static error: string = 'error';
+}
---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../services/user.service';
+import { SnackbarService } from '../services/snackbar.service';
+import { MatDialogRef } from '@angular/material/dialog';
+import { FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { GlobalConstants } from '../shared/global-constant';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss'],
+})
+export class SignupComponent implements OnInit {
+  signupForm: any = FormGroup;
+  responseMessage: any;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private userService: UserService,
+    private snackbarService: SnackbarService,
+    private dialogRef: MatDialogRef<SignupComponent>
+  ) {}
+
+  ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      name: [
+        null,
+        [Validators.required, Validators.pattern(GlobalConstants.nameRegex)],
+      ],
+
+      email: [
+        null,
+        [Validators.required, Validators.pattern(GlobalConstants.emailRegex)],
+      ],
+
+      organisation: [null, [Validators.required]],
+
+      password: [null, [Validators.required]],
+    });
+  }
+
+  handleSubmit() {
+    var formData = this.signupForm.value;
+    var data = {
+      name: formData.name,
+      email: formData.email,
+      organisation: formData.organisation,
+      password: formData.password,
+    };
+    this.userService.signup(data).subscribe(
+      (response: any) => {
+        this.dialogRef.close();
+        this.responseMessage = response?.message;
+        this.snackbarService.openSnackBar(this.responseMessage, '');
+        this.router.navigate(['/']);
+      },
+      (error) => {
+        if (error.error?.message) {
+          this.responseMessage = error.error?.message;
+        } else {
+          this.responseMessage = GlobalConstants.genericError;
+        }
+        this.snackbarService.openSnackBar(
+          this.responseMessage,
+          GlobalConstants.error
+        );
+      }
+    );
+  }
+}
